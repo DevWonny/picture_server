c7791c923ba5f44a5a80c09ee5abf87a5e2efd89
DevWonny <cjfdnjs1994@naver.com>
2022-10-21 09:54:39 +0900

    feat : register api 구현 완료

--- models/user.js
+++ models/user.js
@@ -4,8 +4,10 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    id: { type: String, required: true },
-    password: { type: String, required: true },
+    // unique -> 같은 아이디 방지
+    id: { type: String, required: true, unique: true },
+    hashedPassword: { type: String, required: true },
+    // password: { type: String, required: true },
     introduce: { type: String, required: false },
   },
   {
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,30 @@
 const { Router } = require("express");
 const userRouter = Router();
 const User = require("../models/user");
+// password 암호화 작업
+const { hash } = require("bcryptjs");
 
 userRouter.post("/register", async (req, res) => {
-  console.log(req.body);
-  await new User(req.body).save();
-  res.json({ message: "user register!!" });
+  try {
+    // password가 6자리 이하일 경우
+    if (req.body.password.length < 6) {
+      throw new Error("비밀번호를 6자 이상 입력해주세요!");
+    }
+    // id가 3자리 이하인 경우
+    if (req.body.id.length < 3) {
+      throw new Error("아이디를 3자 이상 입력해주세요!");
+    }
+    // password 암호화
+    const hashedPassword = await hash(req.body.password, 10);
+    await new User({
+      name: req.body.name,
+      id: req.body.id,
+      hashedPassword,
+    }).save();
+    res.json({ message: "user register!!" });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 module.exports = { userRouter };
