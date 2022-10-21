f6a247f8cbd66b0c5777dfdc022838e5989d8fdb
DevWonny <cjfdnjs1994@naver.com>
2022-10-21 10:57:27 +0900

    feat : logout API 구현

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,6 +3,7 @@ const userRouter = Router();
 const User = require("../models/user");
 // password 암호화 작업
 const { hash, compare } = require("bcryptjs");
+const mongoose = require("mongoose");
 
 // register
 userRouter.post("/register", async (req, res) => {
@@ -12,14 +13,14 @@ userRouter.post("/register", async (req, res) => {
       throw new Error("비밀번호를 6자 이상 입력해주세요!");
     }
     // id가 3자리 이하인 경우
-    if (req.body.id.length < 3) {
+    if (req.body.userId.length < 3) {
       throw new Error("아이디를 3자 이상 입력해주세요!");
     }
     // password 암호화
     const hashedPassword = await hash(req.body.password, 10);
     const user = await new User({
       name: req.body.name,
-      id: req.body.id,
+      userId: req.body.userId,
       hashedPassword,
       sessions: [{ createAt: new Date() }],
     }).save();
@@ -37,9 +38,9 @@ userRouter.post("/register", async (req, res) => {
 });
 
 // login
-userRouter.post("/login", async (req, res) => {
+userRouter.patch("/login", async (req, res) => {
   try {
-    const user = await User.findOne({ id: req.body.id });
+    const user = await User.findOne({ userId: req.body.userId });
     const isValid = await compare(req.body.password, user.hashedPassword);
 
     if (!isValid) {
@@ -60,4 +61,32 @@ userRouter.post("/login", async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
+
+// logout
+// 로그인 시 header에 sessionId를 넣고
+userRouter.patch("/logout", async (req, res) => {
+  try {
+    const { sessionid } = req.headers;
+    if (!mongoose.isValidObjectId(sessionid)) {
+      throw new Error("올바르지 않은 session Id 입니다!");
+    }
+
+    const user = await User.findOne({ "sessions._id": sessionid });
+
+    if (!user) {
+      throw new Error("로그인되지 않은 유저입니다.");
+    }
+
+    await User.updateOne(
+      { _id: user.id },
+      { $pull: { sessions: { _id: sessionid } } }
+    );
+
+    // console.log({ user });
+    res.json({ message: "user Logout!" });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
 module.exports = { userRouter };
