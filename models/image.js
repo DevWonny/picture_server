eddff71400645f3bffd9e456f6f5c4db1f90fc05
DevWonny <cjfdnjs1994@naver.com>
2022-10-21 11:24:59 +0900

    feat : 로그인 확인 후 이미지 업로드 api 구현

--- models/image.js
+++ models/image.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 
 const ImageSchema = new mongoose.Schema(
   {
+    user: {
+      _id: { type: mongoose.Types.ObjectId, required: true },
+      name: { type: String, required: true },
+      userId: { type: String, required: true },
+    },
     key: { type: String, required: true },
     originalFileName: { type: String, required: true },
   },
--- routes/imageRouter.js
+++ routes/imageRouter.js
@@ -4,16 +4,30 @@ const { upload } = require("../middleware/imageUpload");
 const imageRouter = Router();
 
 // image upload API
-imageRouter.post("/images", upload.single("imageTest"), async (req, res) => {
-  const image = await new Image({
-    key: req.file.filename,
-    originalFileName: req.file.originalname,
-  }).save();
-  res.json(image);
+imageRouter.post("/", upload.single("imageTest"), async (req, res) => {
+  try {
+    if (!req.user) {
+      throw new Error("권한 없음! 로그인 후 이미지를 업로드 해주세요!");
+    }
+
+    const image = await new Image({
+      user: {
+        _id: req.user.id,
+        name: req.user.name,
+        userId: req.user.userId,
+      },
+      key: req.file.filename,
+      originalFileName: req.file.originalname,
+    }).save();
+    res.json(image);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: err.message });
+  }
 });
 
 // image get API
-imageRouter.get("/images", async (req, res) => {
+imageRouter.get("/", async (req, res) => {
   const images = await Image.find();
   res.json(images);
 });
